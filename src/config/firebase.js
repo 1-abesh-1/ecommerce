7efe22a9944b4b84498027bc1e547a26afbe8590
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2025-03-05 20:02:59 +0600

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ90I5tXuz5SWDKhSR2IAjFbM2VOT4aVI",
-  authDomain: "business-point-a196c.firebaseapp.com",
-  databaseURL: "https://business-point-a196c-default-rtdb.firebaseio.com",
-  projectId: "business-point-a196c",
-  storageBucket: "business-point-a196c.appspot.com",
-  messagingSenderId: "736584703640",
-  appId: "1:736584703640:web:782f8c5afb3036c0b33aed",
-  measurementId: "G-P86Y5XPR91"
+ //firebase config
 };
 
 const app = initializeApp(firebaseConfig);
